1cc9b4c0e9aebd22f66028e37f977596cd8a81ac
Joaquin <joaquin@Radiums-MBP-5.fibertel.com.ar>
2022-10-16 19:28:07 -0300

    dotenv example

---
+++ README.md
@@ -0,0 +1,6 @@
+### class 10
+
+## branches
+
+# dotenv
+In this branch we will be showing how to use the dotenv dependency in a local environment.
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-// import express from 'express';
-require('dotenv').config()
-// const app = express();
 
-console.log(process.env)
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express';
+
+const app = express();
+app.use(express.json())
+
+app.listen(4000, () => console.log(`server listening on port ${process.env.PORT}`))
+
+app.get('/', (req, res) => res.json({message: 'grettings'}))
