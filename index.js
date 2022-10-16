20dfd671429132513bd08ce6da2bed5fbda2d1b9
Joaquin <joaquin@Radiums-MBP-5.fibertel.com.ar>
2022-10-16 19:33:42 -0300

    add vercel config

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-
-
 import dotenv from 'dotenv'
-dotenv.config()
-
 import express from 'express';
 
-const app = express();
-app.use(express.json())
+dotenv.config()
+const app = express().use(express.json())
 
 app.listen(4000, () => console.log(`server listening on port ${process.env.PORT}`))
 
-app.get('/', (req, res) => res.json({message: 'grettings'}))
+app.get('/', (req, res) => res.json({message: `You are ${process.env.MY_NAME} and the environment is ${process.env.VERCEL_ENV}`}))
