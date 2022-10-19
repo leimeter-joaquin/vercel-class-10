7212c187ef053e4269ca83e11f86b401dbe574d6
Joaquin <joaquin@Radiums-MBP-5.fibertel.com.ar>
2022-10-18 23:01:01 -0300

    add validation for port

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 ### class 10
-
-## branches
-
-# dotenv
-In this branch we will be showing how to use the dotenv dependency in a local environment.
+asd
--- index.js
+++ index.js
@@ -4,6 +4,13 @@ import express from 'express';
 dotenv.config()
 const app = express().use(express.json())
 
-app.listen(4000, () => console.log(`server listening on port ${process.env.PORT}`))
+if(process.env.PORT) {
+    app.listen(process.env.PORT, (err) => {
+        console.log(`server listening on port: ${process.env.PORT}`)
+    })
+} else {
+    console.log('No port provided')
+    process.exit()
+}
 
 app.get('/', (req, res) => res.json({message: `You are ${process.env.MY_NAME} and the environment is ${process.env.VERCEL_ENV}`}))
