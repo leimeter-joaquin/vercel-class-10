f5bbfb65b1c5127d8d985551013d39c513816f9c
leimeter-joaquin <leimeter.joaquin@gmail.com>
2023-05-16 16:53:55 -0300

    add guard to not listen a port on development

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import dotenv from 'dotenv'
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
 
-dotenv.config()
-const app = express().use(express.json())
+dotenv.config();
+const app = express().use(express.json());
 
-if(process.env.PORT) {
-    app.listen(process.env.PORT, (err) => {
-        console.log(`server listening on port: ${process.env.PORT}`)
-    })
-} else {
-    console.log('No port provided')
-    process.exit()
+try {
+  app.listen(process.env.PORT, (err) => {
+    if (err) throw err;
+    console.log(`server listening on port: ${process.env.PORT}`);
+  });
+} catch (err) {
+  console.log("There was an error starting the server: ", err);
 }
 
-app.get('/', (req, res) => res.json({message: `You are ${process.env.MY_NAME} and the environment is ${process.env.VERCEL_ENV}`}))
+app.get("/", (req, res) =>
+  res.json({
+    message: `You are ${process.env.MY_NAME} and the environment is ${process.env.VERCEL_ENV}`,
+  })
+);
